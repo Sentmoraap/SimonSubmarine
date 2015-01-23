fd30c50f5826345a07aed646cb88769fadc19dc9
Lilian Gimenez <lilian.gimenez@laposte.net>
2015-01-24 00:54:17 +0100

    Base code of IA, Submarine and Object class

---
+++ Source/Assets/Scripts/Gameplay/IA.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IA : MonoBehaviour
+{
+    // TODO GD : set AI'S initial appreciation
+
+    #region PrivateAttributes
+    private float m_appreciation; // AI's appreciation in [0;1]
+
+    #region MonoBehaviour
+    void Start ()
+    {
+        m_appreciation = 0.5f;    
+	}
+	
+	void Update ()
+    {
+
+    }
+    #endregion
+
+    #region methods
+    public void SimonSays()
+    {
+        // TODO GD : how to generate orders
+        // TODO : generate orders
+    }
+
+    public void ModifyAppreciation(float diff)
+    {
+        m_appreciation = Mathf.Clamp(m_appreciation+diff, 0, 1);
+    }
+    #endregion
+}
---
+++ Source/Assets/Scripts/Gameplay/Object.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class Object : MonoBehaviour
+{
+    #region publicAttributes
+    public bool _isActivated;
+    public bool _isMovable;
+    #endregion
+
+    
+    #region MonoBehaviour
+    void Start ()
+    {
+	    _isActivated=flase;
+        _isMovable=false;
+	}
+	
+	void Update ()
+    {
+        doCommonStuff();
+        if(_isActivated) doActivatedStuff(); else doDisactivatedStuff();
+    }
+    #endregion
+
+    #region virtualMethods
+    protected virtual void doCommonStuff() {};
+    protected virtual void doActivatedStuff() {};
+    protected virtual void doDisactivatedStuff(){};
+
+    #endregion
+}
