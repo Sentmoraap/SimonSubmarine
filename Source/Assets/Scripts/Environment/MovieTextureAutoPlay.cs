38d4054e9cd251208146083d84ce7a7073246862
unknown <romain.vivier@STUD-VDOC-051.formation.enjmin.fr>
2015-01-25 00:58:07 +0100

    Mise en place du script pour la salle cine

---
+++ Source/Assets/Scripts/Environment/MovieTextureAutoPlay.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovieTextureAutoPlay : ActionObject {
+
+
+    public Material[] _movietextures;
+
+    private int m_current;
+    private MovieTexture m_mt;
+
+	public override void Start ()
+    {
+
+        m_current = 1;
+        m_mt = (MovieTexture)renderer.material.mainTexture;
+        m_mt.Play();
+
+	}
+
+    protected override void activateAction()
+    {
+        base.activateAction();
+
+        if(m_current == _movietextures.Length-1)
+        {
+            m_current = 0;
+        }
+        else
+        {
+            m_current ++;
+        }
+
+        renderer.material = _movietextures[m_current];
+        m_mt = (MovieTexture)renderer.material.mainTexture;
+        m_mt.Play();
+        
+    }
+
+}
