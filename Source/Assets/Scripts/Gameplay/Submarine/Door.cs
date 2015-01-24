567e1467de05a17d84581ea588d1bd4f351d86e9
unknown <romain.vivier@STUD-VDOC-051.formation.enjmin.fr>
2015-01-24 01:06:14 +0100

    Ajout des classes Room et Door et mise à jour du gitignore

---
+++ Source/Assets/Scripts/Gameplay/Submarine/Door.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Door : MonoBehaviour {
+
+#region members
+
+    public Room[] m_rooms;
+
+    private DoorState m_doorState;
+    private float m_pressure;
+    private float m_leak;
+
+#endregion
+
+#region Properties
+
+    public DoorState DoorState { get { return m_doorState; } set { m_doorState = value; } }
+
+    public float Pressure { get { return m_pressure; } }
+
+    public float Leak { get { return m_leak; } }
+
+
+#endregion
+
+#region Mono Functions
+
+    void Start()
+    {
+        DoorState = DoorState.Locked;
+    }
+
+    void Update()
+    {
+        if(DoorState == DoorState.Locked)
+        {
+            UpdatePressure();
+        }
+    }
+
+#endregion
+
+#region Class Functions
+
+    private void UpdatePressure()
+    {
+        //TODO Update room water or heat value considering the leak 
+    }
+
+#endregion
+
+}
+
+public enum DoorState
+{
+    Open,
+    Closed,
+    Locked
+}
---
+++ Source/Assets/Scripts/Gameplay/Submarine/Room.cs
@@ -0,0 +1,86 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Room : MonoBehaviour {
+
+#region members
+
+    public int _floor;
+
+    private float m_health;
+    //private List<Object> m_objects;
+    private List<Door> m_doors;
+    
+    private float m_waterValue;
+    private float m_heatValue;
+
+    private bool m_isVisible;
+
+#endregion 
+
+#region Properties
+
+    public float Health { get { return m_health; } set { m_health = value; } }
+
+    public List<Door> Doors { get { return m_doors; } }
+
+    public float WaterValue { get { return m_waterValue; } }
+
+    public float HeatValue { get { return m_heatValue; } }
+
+    public bool IsVisible
+    { 
+        get { return m_isVisible; } 
+        
+        set
+        { 
+            m_isVisible = value;
+            //TODO active a black plane when not visible
+        }
+    }
+
+#endregion 
+
+#region Mono Functions
+
+    void Start ()
+    {
+	
+	}
+	
+	void Update ()
+    {
+        //TODO each of them might be update only considering a certain state of the room
+        UpdateHealth();
+        UpdateWaterValue();
+        UpdateHeatValue();
+	}
+
+#endregion 
+
+#region Class Functions
+
+    private void UpdateHealth()
+    {
+        //TODO Update room health considering water and heat value
+    }
+
+    private void UpdateWaterValue()
+    {
+        //TODO Update water value considering water propagation
+    }
+
+    private void UpdateHeatValue()
+    {
+        //TODO Update Heat value considering Heat propagation
+    }
+
+#endregion
+
+}
+
+public enum RoomState
+{
+    //TODO must be define with game designers
+}
