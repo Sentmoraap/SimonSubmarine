26267ebaaefefcc3e2a14df70cca933a5af9258a
Lilian Gimenez <lilian.gimenez@laposte.net>
2015-01-24 13:13:11 +0100

    Room unlocking by phase

--- Source/Assets/Scripts/Gameplay/Submarine/Submarine.cs
+++ Source/Assets/Scripts/Gameplay/Submarine/Submarine.cs
@@ -7,7 +7,6 @@ public class Submarine
     #region publicAttributes
     // The rooms must be ordered in unlock order
     public List<Room> _rooms;
-    public int _unlockedRooms=0; // If unlockedRooms is x, the player can go in rooms [0;x-1]
     #endregion
 
     #region properties
@@ -18,24 +17,51 @@ public class Submarine
             return m_health;
         }
     }
+
+    public int UnlockedRooms
+    {
+        get
+        {
+            return m_unlockedRooms;
+        }
+    }
     #endregion
 
     #region privateAttributes
+    private int m_unlockedRooms=0;
+    private int m_phase = 0;
     private float m_health; // Global health in [0;1]
+    private List<int> m_phaseRoomUnlocks;
     #endregion
 
     #region methods
+
+    public Submarine()
+    {
+        m_phaseRoomUnlocks=new List<int>();
+        m_phaseRoomUnlocks.Add(3);
+        m_phaseRoomUnlocks.Add(6);
+        m_phaseRoomUnlocks.Add(10);
+    }
+
     public void UpdateHealth()
     {
         //TODO when we have rooms
     }
 
     // Can be used with a negative number to lock rooms
-    public void UnlockRooms(int nbRooms)
+    /*public void UnlockRooms(int nbRooms)
     {
         _unlockedRooms += nbRooms;
         if(_unlockedRooms<0) _unlockedRooms=0;
         if (_unlockedRooms > _rooms.Count - 1) _unlockedRooms = _rooms.Count - 1;
+    }*/
+
+    public void UnlockNextPhase()
+    {
+        m_phase++;
+        m_unlockedRooms = m_phaseRoomUnlocks[m_phase];
     }
+
     #endregion
 }
