582d137e2955bf0facc878ecd1508d59d0d504ab
unknown <romain.vivier@STUD-VDOC-051.formation.enjmin.fr>
2015-01-24 15:10:16 +0100

    porte okay !

--- Source/Assets/Scripts/Gameplay/Character.cs
+++ Source/Assets/Scripts/Gameplay/Character.cs
@@ -91,8 +91,6 @@ public class Character : MonoBehaviour
                 m_objects.Add(aObj);
             }
         }
-
-        Debug.Log(m_objects.Count);
         //return cols.Length > 0;
     }
 
@@ -105,7 +103,6 @@ public class Character : MonoBehaviour
 
     void ActionDown()
     {
-        Debug.Log("ActionDown");
         if(m_objects.Count > 0)
         {
             m_objects[0].activateActionDown();
@@ -114,7 +111,6 @@ public class Character : MonoBehaviour
 
     void ActionUp()
     {
-        Debug.Log("ActionUp");
         if(m_objects.Count > 0)
         {
             m_objects[0].activateActionUp();
