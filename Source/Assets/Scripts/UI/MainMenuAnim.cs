e74e636e8e02c723ff475c8330bdea229f029006
Shu <bigx_jojo@hotmail.com>
2015-01-25 14:04:52 +0100

    UI

---
+++ Source/Assets/Scripts/UI/MainMenuAnim.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenuAnim : MonoBehaviour {
+    public Animator cr;
+	// Use this for initialization
+	void Start () {
+        cr.SetBool("Walk", true);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
